d431128494caa2265f153d04e1abf0d9ea32360f
wkmkymt <wkmkymt@gmail.com>
2017-03-21 15:08:48 +0900

    [fix] Azure SQL Databaseに接続出来るように修正

--- api_history.php
+++ api_history.php
@@ -1,46 +1,58 @@
-<?php 
-/*
-  $db['host'] = "localhost";  // DB server's url
-  $db['user'] = "daichi";
-  $db['pass'] = "yoshitake";
-  $db['dbname'] = "aktiva-archive";
-*/
+<?php
 
-  $db['host'] = "host";  // DB sever's url
-  $db['user'] = "user";
-  $db['pass'] = "pass";
-  $db['dbname'] = "dbname";
+  $connectionInfo = [
+    "UID"                    => "atsushi@aktiva-archive",
+    "pwd"                    => "Shigoto4510",
+    "Database"               => "aktiva-archive-db",
+    "CharacterSet"           => "UTF-8"
+  ];
+  $serverName = "tcp:aktiva-archive.database.windows.net,1433";
 
+  $conn = sqlsrv_connect($serverName, $connectionInfo);
+  if($conn === false) {
+    print('<p>データベースへの接続に失敗しました。</p>');
+    foreach(sqlsrv_errors() as $error)
+      print("Error:  " . $error["message"]);
+    sqlsrv_close($conn);
+    exit();
+  }
 
-
-  $mysqli = new mysqli($db['host'], $db['user'], $db['pass']);
-  if ($mysqli->connect_errno) {
-      print('<p>データベースへの接続に失敗しました。</p>' . $mysqli->connect_error);
-      exit();
-    }
-
-  $mysqli->select_db($db['dbname']);
-
-  $userid = $_GET["user"];
+  $userid = utf8_encode($_GET["user"]);
   $num_fetch = 2; //number of scores fetched from the database
 
   //Get the max number of music id
-  $query = "SELECT MAX(music_id) FROM music";
-  $result = $mysqli->query($query);
-  $num_music = $result->fetch_assoc()["MAX(music_id)"];
+  $query = "SELECT MAX(music_id) FROM \"music\"";
+  $result = sqlsrv_query($conn, $query)
+  if($result === false) {
+    print('<p>クエリーが失敗しました。</p>');
+    foreach(sqlsrv_errors() as $error)
+      print("Error:  " . $error["message"]);
+    sqlsrv_close($conn);
+    exit();
+  }
+  $num_music = sqlsrv_fetch_array($result)["0"];
 
   for ($i = 1; $i <= $num_music; $i++) {
     $j = 0;
     $text = $i;
-    $query = "SELECT * FROM history WHERE user_id = '". $userid . "'  AND music_id = '". $i ."' order by history_id DESC LIMIT ". $num_fetch;
-    $result = $mysqli->query($query);
-    while ($row = $result->fetch_assoc()) { 
+
+    $query = "SELECT * FROM \"history\" WHERE user_id LIKE '". $userid . "'  AND music_id LIKE '". $i ."' order by history_id DESC OFFSET 0 ROWS FETCH NEXT ". $num_fetch." ROWS ONLY";
+    $result = sqlsrv_query($conn, $query)
+    if($result === false) {
+      print('<p>クエリーが失敗しました。</p>');
+      foreach(sqlsrv_errors() as $error)
+        print("Error:  " . $error["message"]);
+      sqlsrv_close($conn);
+      exit();
+    }
+
+    while ($row = sqlsrv_fetch_array($result)) {
       if ($j < $num_fetch) {
         $text .= "||" . $row["score"];
         $j++;
       }
     }
-    for (; $j < $num_fetch; $j++) { 
+    for (; $j < $num_fetch; $j++) {
       $text .= "||-1";
     }
     echo $text;
